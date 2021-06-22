d5a61d47b0c3f946bfd1fbc06c9a1623e8498b65
zergal <kalentinvs@gmail.com>
2021-06-22 17:58:35 +0300

    Revert "Actively working on SDP negotiation in web/Call/Call.js"
    This reverts commit 722e19fd8a0016eb92f88590a3de62ce5e26d828.

--- web/Call/Call.js
+++ web/Call/Call.js
@@ -5,30 +5,13 @@ const stream = navigator.mediaDevices.getUserMedia(
 stream.then(streamlet => {
     document.querySelector(`${config.namespace} > #call`).srcObject = streamlet;
 })
-pc1 = new RTCPeerConnection(null);
-pc2 = new RTCPeerConnection(null);
-
-
-pc1.createOffer({
-    offerToReceiveAudio: 1,
-    offerToReceiveVideo: 1
-}).then(
-    (desc1) => {
-
-        pc1.setLocalDescription(desc1)
-        pc2.setRemoteDescription(desc1);
-
-        pc2.createAnswer((desc2) => {
-            pc2.setLocalDescription(desc2);
-            pc1.setRemoteDescription(desc2)
-        })
-        pc1.onicecandidate = (candidate) => {
-            console.log(candidate)
-            pc1.addIceCandidate(candidate.candidate)
-        };
-        pc2.onicecandidate = (candidate) => {
-            console.log(candidate)
-            pc2.addIceCandidate(candidate.candidate)
-        };
-    }
-)
+pc = new RTCPeerConnection();
+sg = new WebSocket('ws://128.0.128.26:8000');
+sg.onmessage = (msg) => {
+    console.log(msg)
+};
+sg.onopen = (e) => {
+    console.log(e)
+    sg.send('JOIN.conn');
+    sg.send(`SEND.conn A`);
+}
