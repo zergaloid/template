329d265b4f0e122b39b31a890c9ccac38945b704
zergal <kalentinvs@gmail.com>
2021-06-23 09:58:52 +0300

    rewrote Call/Call.js

--- node/src/ix.js
+++ node/src/ix.js
@@ -5,7 +5,7 @@ const https = require('https');
 const ws = require('./methods/ws');
 
 const server = https.createServer({
-    
+
     // self-signed Certificate
     cert: fs.readFileSync(process.env.PATH_TO_CERT),
     key: fs.readFileSync(process.env.PATH_TO_KEY),
@@ -17,26 +17,33 @@ ws.config({
     // WS server handler
     handler: (sock) => {
         sock.ws.on('message', (message) => {
-            let call = message.split(' ')[0]
-            let cmd =
-            {
-                call: call.split('.'),
-                arg: message.slice(call.length + 1)
-            };
-            switch (cmd.call[0]) {
-                case 'SEND':
-                    sock.wss.Channel.sendTo(cmd.call[1], `RECV.${cmd.call[1]} ${cmd.arg}`)
-                    break;
-                case 'JOIN':
-                    sock.wss.Channel.addTo(cmd.call[1], sock.ws._socket.server.sessionIdContext)
-                    break;
+            if (Buffer.isBuffer(message)) {
+                sock.wss.Channel.sendTo('stream', message)
+            }
+            else {
+                let call = message.split(' ')[0]
+                let cmd =
+                {
+                    call: call.split('.'),
+                    arg: message.slice(call.length + 1)
+                };
+                switch (cmd.call[0]) {
+                    case 'SEND':
+                        sock.wss.Channel.sendTo(cmd.call[1], `RECV.${cmd.call[1]} ${cmd.arg}`)
+                        break;
+                    case 'JOIN':
+                        sock.wss.Channel.addTo(cmd.call[1], sock.ws._socket.server.sessionIdContext)
+                        break;
+                }
             }
         });
     }
 })
 
+
+
 const port = process.env.WS_PORT || obj.port || 8080
 server.listen(port)
 
-if(process.env.NODE_ENV == 'development')
+if (process.env.NODE_ENV == 'development')
     console.info(`port: ${port}`, `server: '${server.address().address}'`)
--- web/Call/Call.js
+++ web/Call/Call.js
@@ -1,23 +1,8 @@
 const socket = new WebSocket('wss://localhost:8000')
 
-socket.onmessage = e => console.log(e)
-
 const stream = navigator.mediaDevices.getUserMedia(
     config.constraints
 ).then(streamlet => {
-    var recorder = new MediaRecorder(streamlet);
     let video = document.querySelector(`${config.namespace} > #call`);
-
-    // recorder.start(30);
-    // setInterval(() => {
-    //     recorder.requestData()
-    // }, 3000);
-    // recorder.ondataavailable = function (e) {
-    //     console.log(e.data)
-    // }
-
-    socket.send('JOIN.stream')
-    socket.send('SEND.stream A')
-
-    video.srcObject = streamlet;
+    
 })
