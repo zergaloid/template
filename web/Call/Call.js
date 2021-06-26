ed7dc9dd30ea94e9feb11e220a50615f65b3a4f6
zergal <kalentinvs@gmail.com>
2021-06-26 18:46:27 +0300

    More unrelated changes in Call/Call dyn, adding 'selectors', in essenc, room/role definitions

--- web/Call/Call.js
+++ web/Call/Call.js
@@ -1,10 +1,20 @@
 let socket = new WebSocket(config.wss);
-
-let channel = config.channel;
+let selector = config.selector.split('/');
+selector = {
+  room: selector[0],
+  role: selector[1]
+}
 let video = document.querySelector(`${config.namespace} > video`)
 
 socket.onopen = async () => {
-    socket.send(`JOIN.${channel}`)
+    socket.send(`JOIN.${selector.room}`)
+    switch(selector.role)
+    {
+      case "caller":
+        break;
+      case "callee":
+        break;
+    }
 }
 
 navigator.mediaDevices.getUserMedia(config.constraints).then((streamlet) => {
