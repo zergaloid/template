67a1ab3bff8f0df5b7947ab9f12d55d11a4d965b
zergal <kalentinvs@gmail.com>
2021-06-29 01:10:19 +0300

    Got SDP to work

--- web/Call/Call.js
+++ web/Call/Call.js
@@ -1,5 +1,5 @@
 let socket = new WebSocket(config.wss);
-const peer = new RTCPeerConnection();
+const peer = new RTCPeerConnection({ 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] });
 
 // Room/Role selector, specifies both
 let selector = config.selector.split('/');
@@ -7,36 +7,93 @@ selector = {
   room: selector[0],
   role: selector[1]
 }
-// RECV-trimming function
-let trim = (s) => s.slice(`RECV.${selector.room} `.length)
+
+let helpers = {
+  // RECV-trimming function
+  trim: (s) => s.slice(`RECV.${selector.room}_x `.length),
+  offer: (o) => socket.send(`SEND.${selector.room}_o ${JSON.stringify(o)}`),
+  answer: (a) => socket.send(`SEND.${selector.room}_a ${JSON.stringify(a)}`),
+  ice: (i) => socket.send(`SEND.${selector.room}_i ${JSON.stringify(i)}`)
+}
+
 // Local video <element>
 let video = document.querySelector(`${config.namespace} > video`)
 
 switch (selector.role) {
   case 'caller':
     socket.onopen = async () => {
-      let offer = await peer.createOffer()
-      offer = JSON.stringify(offer)
+      socket.send(`JOIN.${selector.room}_a`)
 
-      socket.send(`SEND.${selector.room} ${offer}`)
+      let offer = await peer.createOffer({
+        'offerToReceiveAudio': false,
+        'offerToReceiveVideo': true
+      })
+      peer.setLocalDescription(new RTCSessionDescription(offer))
+
+      helpers.offer(offer)
     }
     break;
   case 'callee':
-    socket.onopen = async () =>{
-      socket.send(`JOIN.${selector.room}`)
+    socket.onopen = async () => {
+      socket.send(`JOIN.${selector.room}_o`)
     }
     break;
 }
 
-socket.onmessage = async (e) => {
-  // offer = trim(e.data)
-  // offer = JSON.parse(offer)
-  
-  console.log(e)
+peer.addEventListener('icecandidate', event => {
+  console.log(event)
+  if (event.candidate) {
+    helpers.ice(event.candidate);
+  }
+});
 
-  // peer.setRemoteDescription(offer)
+
+socket.onmessage = async (e) => {
+  offer = helpers.trim(e.data)
+  offer = JSON.parse(offer)
+
+  await peer.setRemoteDescription(new RTCSessionDescription(offer))
+  switch (selector.role) {
+    case 'callee':
+      let answer = await peer.createAnswer();
+      await peer.setLocalDescription(new RTCSessionDescription(answer));
+      helpers.answer(answer)
+      break;
+    case 'caller':
+      socket.send(`JOIN.${selector.room}_i`)
+      socket.onmessage = async (se) => {
+        icec = helpers.trim(se.data)
+        icec = JSON.parse(icec)
+
+        try {
+          await peer.addIceCandidate(icec);
+        } catch (e) {
+          console.error('Error adding received ice candidate', e, icec);
+        }
+      }
+      break;
+  }
 }
 
 navigator.mediaDevices.getUserMedia(config.constraints).then((streamlet) => {
-  video.srcObject = streamlet;
+  if (selector.role == 'caller') {
+    video.srcObject = streamlet;
+  }
+  peer.oniceconnectionstatechange = function (event) {
+    if (peer.iceConnectionState == 'connected' && selector.role == 'caller') {
+      setTimeout(() => {
+        console.log(streamlet)
+        streamlet.getVideoTracks().forEach((track, i, a) => {
+          console.log(track)
+          peer.addTrack(track, streamlet);
+        });
+      }, 300);
+    }
+    console.log(peer)
+  };
+
+  peer.ontrack = async (event) => {
+    console.log(event)
+    video.srcObject = event.streams[0];
+  };
 })
