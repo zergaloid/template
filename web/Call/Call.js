3d739741d68e5195a1744be6b1349654ebc1866d
zergal <kalentinvs@gmail.com>
2021-06-28 14:37:38 +0300

    Major web/ code cleanup

--- web/Call/Call.js
+++ web/Call/Call.js
@@ -10,34 +10,10 @@ selector = {
 let trim = (s) => s.slice(`RECV.${selector.room} `.length)
 // Local video <element>
 let video = document.querySelector(`${config.namespace} > video`)
-let link = ''
-var xhr = new XMLHttpRequest();
 
 socket.onopen = async () => {
     socket.send(`JOIN.${selector.room}`)
 }
-
-socket.onmessage = async(e) => {
-    message = trim(e.data).split('/')
-    message = {
-      to: message[0],
-      content: message[1]
-    }
-    if(message.to == selector.role)
-    {
-      vp8 = JSON.parse(message.content)
-      console.log(vp8)
-    }
-}
-
 navigator.mediaDevices.getUserMedia(config.constraints).then((streamlet) => {
-    let recorder = new MediaRecorder(streamlet);
-    recorder.start(30)
-    if(selector.role == 'caller')
-    recorder.ondataavailable = async (e) => {
-      data = await e.data.arrayBuffer()
-      data = new Uint8Array(data)
-      data = JSON.stringify(data)
-      socket.send(`SEND.${selector.room} callee/${data}`)
-    }
+  video.srcObject = streamlet;
 })
