da0d9d3f8c616922c9df263a29658711f2b71bcd
Valentin Popov <kalentinvs@gmail.com>
2021-09-08 13:32:27 +0300

    Major cleanup, now using Gulp+Coffescript

---
+++ Gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const browserify = require('browserify')
+const uglify = require('gulp-uglify')
+
+const gulp = require('gulp');
+const { watch, series } = gulp
+
+const through = require('through2')
+const source = require('vinyl-source-stream')
+const buffer = require('vinyl-buffer')
+const globby = require('globby')
+
+let output = "./public/"
+let sources =
+{
+  coffee: [
+    "./src/*.coffee"
+  ],
+  browser: [
+    "."
+  ]
+}
+
+function coffee() {
+  gulp.src(sources.coffee[0])
+    .pipe(require('gulp-coffee')({ bare: true }))
+    .pipe(gulp.dest(output));
+
+    var stream = through()
+
+    stream
+      .pipe(source('app.js'))
+      .pipe(buffer())
+      .pipe(uglify())
+      .pipe(gulp.dest('./public/js/'))
+  
+    globby(["./public/*.js"]).then((files) => {
+      let b = browserify({
+        entries: files,
+        debug: true
+      });
+  
+      b.bundle().pipe(stream);
+    }).catch((e) => {
+      stream.emit('error', e)
+    })
+    return stream;
+}
+
+exports.default = function () {
+  watch(sources.coffee[0], coffee)
+}
