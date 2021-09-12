8a50f67473021bfdbed62dba116c4c77d0919f21
Valentin Popov <kalentinvs@gmail.com>
2021-09-12 20:38:26 +0300

    Added app.html

---
+++ src/sw.js
@@ -0,0 +1,8 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+
+workbox.routing.registerRoute(
+  new RegExp('*'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'cache',
+  })
+);
