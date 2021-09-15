0ccc9795b9ea4e4a6bdca33c876223f4d3f0eedd
Valentin Popov <kalentinvs@gmail.com>
2021-09-15 23:51:49 +0300

    template ready

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,21 @@
 'use strict';
 
 const gulp = require('gulp');
-const { watch, series } = gulp
+const { watch, src, dest } = gulp
 
 let output = "./public/"
 let sources =
 {
+  img: [
+    "./src/img/*"
+  ],
   js: [
-    "./src/*.js",
-    "./src/*.json"
+    "./src/*.json",
+    "./src/*.js"
   ],
   html: [
-    "./src/html/*.html",
-    "./src/html/*/*.html"
+    "./src/html/*/*.html",
+    "./src/html/*.html"
   ],
   css: [
     "./src/css/*.css",
@@ -22,47 +25,54 @@ let sources =
 
 function css() {
   const postcss = require('gulp-postcss')
-
-  gulp.src(sources.css[0])
+  src(sources.css[0])
     .pipe(postcss([require('autoprefixer'), require('postcss-nested')]))
-    .pipe(gulp.dest('./public/css'))
+    .pipe(dest(`${output}/css`))
 
-  return gulp.src(sources.css[1])
+  return src(sources.css[1])
     .pipe(postcss([require('autoprefixer'), require('postcss-nested')]))
-    .pipe(gulp.dest('./public/css'))
+    .pipe(dest(`${output}/css`))
+}
+
+function img() {
+  const image = require('gulp-image')
+  return src(sources.img[0])
+    .pipe(image())
+    .pipe(dest(`${output}/img`))
 }
 
 function html() {
-  const image = require('gulp-image')
-  const importer = require('gulp-html-import');
+  const importer = require('gulp-web-include');
+  
+  src(sources.html[0])
+    .pipe(importer('./src/html/', "html"))
+    .pipe(dest(output));
 
-
-  gulp.src('src/img/*')
-    .pipe(image())
-    .pipe(gulp.dest('public/img/'))
-
-
-  return gulp.src(sources.html[0])
-    .pipe(importer('./src/html/'))
-    .pipe(gulp.dest(output));
+  return src(sources.html[1])
+    .pipe(importer('./src/html/', "html"))
+    .pipe(dest(output));
 }
 
 function js() {
   const uglify = require('gulp-uglify')
 
-  gulp.src('src/*.json')
-    .pipe(gulp.dest('public/'))
+  src(sources.js[0])
+    .pipe(dest(output))
 
-  return gulp.src(sources.js[0])
+  return src(sources.js[1])
     .pipe(uglify())
-    .pipe(gulp.dest(output));
+    .pipe(dest(output));
 }
 
 exports.default = function () {
+  // JS and JSON
   watch(sources.js[0], js)
   watch(sources.js[1], js)
+  // HTML and it's includes
   watch(sources.html[0], html)
   watch(sources.html[1], html)
+  // CSS and images
   watch(sources.css[0], css)
   watch(sources.css[1], css)
+  watch(sources.img[0], img)
 }
--- src/ix.js
+++ src/ix.js
@@ -1,32 +1,3 @@
-var link = window.location.pathname.split("/");
-link = link[link.length - 1]
-
-
-window.onload = () => {
-    switch (link) {
-        case 'whatisthis.html':
-            let input = document.getElementById('join-input');
-            let form = document.getElementById('try-it');
-
-            form.addEventListener('submit',
-                () => {
-                    input.value = "You're in the waitlist!";
-                    input.disabled = true;
-                })
-            input.onchange = input.oninput = function () {
-                form.action = `https://functions.yandexcloud.net/d4edsioq0odbmd9blij6?email=${input.value}`
-            }
-            break;
-        default:
-            break;
-    }
-    // if (link !== "app.html") {
-    //     let button = document.getElementById('app-button')
-    //     button.onclick = () => {
-    //         window.location.assign('/app.html')
-    //     }
-    // }
-}
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js')
