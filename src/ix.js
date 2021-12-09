672d8e8c4e6a7a9d253b5aea9a65f69bfd1cb94f
Valentin Popov <kalentinvs@gmail.com>
2021-12-09 18:13:34 +0300

    Fixed some problems caused by package breaking changes

--- src/ix.js
+++ src/ix.js
@@ -1,4 +1,4 @@
-@include ("https://unpkg.com/page/page.js")
+@include ("https://cdnjs.cloudflare.com/ajax/libs/page.js/1.11.6/page.js")
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
     .catch((err) => console.error(err));
